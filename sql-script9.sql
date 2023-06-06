a6556cdc7ee92fa3be50c71e117921b215686e19
Ergin <ergintiravoglu@msn.com>
2023-06-07 02:57:05 +0300

    sql ödev 9 eklendi

---
+++ sql-script9.sql
@@ -0,0 +1,10 @@
+select city.city, country.country from city inner join country on country.country_id = city.country_id;
+
+
+select customer.first_name, customer.last_name from customer 
+inner join payment on payment.customer_id=customer.customer_id;
+
+
+select rental.rental_id, customer.first_name, customer.last_name from customer 
+inner join rental on rental.customer_id = customer.customer_id;
+
